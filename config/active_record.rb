7c549a94a49ee9183fa1f1a1959483f499a6a9e7
Pat Allan <pat@freelancing-gods.com>
2011-05-24 13:43:40 +1000

    Environments are probably going to be strings, not symbols.

--- config/active_record.rb
+++ config/active_record.rb
@@ -1,4 +1,4 @@
 config = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(
-  config[Sinatra::Application.environment]
+  config[Sinatra::Application.environment.to_s]
 )
